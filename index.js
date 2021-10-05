4483a0935ff7e54d700709a1945a7fad8ce3a28a
Alena.Kygai <alena.kygai@gmail.com>
2021-10-05 15:23:01 +0300

    Example hardcode

---
+++ index.js
@@ -0,0 +1,13 @@
+// alert(20);
+// window.alert("hello world");
+
+
+// console
+// hardcode
+console.log(1);
+console.log(undefined);
+console.log('test');
+console.log('1');
+console.log('null');
+console.log(true);
+console.log(false);
