c2ff66f2f02c14dbacdd595f31990c6b5e905bad
Alena.Kygai <alena.kygai@gmail.com>
2021-10-05 15:39:39 +0300

    Create variable

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-// alert(20);
-// window.alert("hello world");
+//variable
+/* 
+имя переменной
+начинается только с буквы или  _ или $
+могут быть цифры в середине или в конце
+регистрозависимое 
+не может быть пробелов в имени переменных
+camelCase
+отвечает на вопрос Что в ней?
+*/
+// создание переменной
+let num1;
+// инициализация 
+num1 = 33;
+// логирование 
+console.log('num1 = ', num1);
 
+let num2 = 28;
 
-// console
-// hardcode
-console.log(1);
-console.log(undefined);
-console.log('test');
-console.log('1');
-console.log('null');
-console.log(true);
-console.log(false);
+num1 = 44;
+console.log('num1 = ', num1);
+
+const num3 = 77;
+console.log('num3 = ', num3);
+
+{
+  let num3 = 'qwerty';
+  console.log('num3 = ', num3);
+}
