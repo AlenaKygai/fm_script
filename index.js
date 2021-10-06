866ac865a80c4be388b478f643b653770c035e1e
Alena.Kygai <alena.kygai@gmail.com>
2021-10-06 18:59:03 +0300

    Example of logic operators

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-let num1 = 10;
-console.log(num1>12);
+/*  
+логическое сложение ИЛИ   || 
+возвращает первую правду или последнюю ложь 
+1+0=1 1+1=2 0+1=1 0+0=0
 
-// console.log(num1>=10);  true
-// console.log(num1<=10); true 
-// console.log(num1<10);  false
+*/ 
 
-// console.log('test'>'best'); true 
-// console.log('test'>'text'); false 
-// console.log('test'>'z'); false 
-// console.log('test'>'Z'); true 
+/*  
+логическое умножение И   && 
+возвращает последнюю правду или первую ложь 
+больше приоритет чем у ИЛИ
+1*0=0 0*1=0 0*0=0 1*1=1 
+
+*/ 
+let cond = 10 && 'Elon';
+console.log(cond);
+
+let cond_1 = false ||  0;
+console.log(cond_1);
+
+
+
+let number = 100;
+let min = 10;
+let max = 20;
+
+if (number<min || number>max){
+  console.log('YOU WIN!', number);
+}
+
+
+// if(number>=min && number<=max){
+//   console.log('YOU WIN!', number);
+// }
