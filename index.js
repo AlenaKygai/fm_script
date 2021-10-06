06a9a78311d9facddeaea62b7b8a2bd80935febd
Alena.Kygai <alena.kygai@gmail.com>
2021-10-06 17:44:50 +0300

    Add remainder operator

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-/* 
-type of false:
-
-false
-0 
-'' //пустая строка
-Nan 
-null
-undefined
-*/
-
-
-
-
-let condition = '12'+ 0;
-console.log(condition, typeof condition);
-
-if (condition){
-  console.log(condition);
-}
+//alert(77);
+let num = 78;
+console.log(num%3);
 
