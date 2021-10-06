5d226f7adeb018bb675429f952f707bfd8e0b5bb
Alena.Kygai <alena.kygai@gmail.com>
2021-10-06 18:34:13 +0300

    Example of compare operations

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-let number = '1';
-console.log(+number);
-// не явное приведение к числу 
+let num1 = 10;
+console.log(num1>12);
 
+// console.log(num1>=10);  true
+// console.log(num1<=10); true 
+// console.log(num1<10);  false
+
+// console.log('test'>'best'); true 
+// console.log('test'>'text'); false 
+// console.log('test'>'z'); false 
+// console.log('test'>'Z'); true 
