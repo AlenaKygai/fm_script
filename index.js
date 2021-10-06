76b97a8a76fd74e54200e501737dab9626b81e8a
Alena.Kygai <alena.kygai@gmail.com>
2021-10-06 17:56:47 +0300

    Add ekran sumbol

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-//alert(77);
-let num = 78;
-console.log(num%3);
+let messUser = 'Маша говорит:\n\'Привет\' ';
 
+console.log(messUser);
+
+let messUser_1 = 'Маша говорит:\t\'Привет\' ';
+
+console.log(messUser_1);
+
+
+let messUser_2 = 'Маша говорит:\u2721\t\'Привет\' ';
+
+console.log(messUser_2);
