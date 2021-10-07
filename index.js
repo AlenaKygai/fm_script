4be9d71089190b8690b239a10cd38f83495d0cc6
Alena.Kygai <alena.kygai@gmail.com>
2021-10-07 19:13:25 +0300

    Add fuction sum - bad expirience

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+/*
 function myFirstFunction(){
   console.log('text in myFirstFunction');
 }
@@ -13,3 +13,19 @@ mySecondFunction();
 
 console.log(mySecondFunction);
 
+*/
+
+const getSumTwoNums = function(){
+  let userInput1 = prompt('enter fisrt number');
+  let userInput2 = prompt('enter second number');
+
+  let checkEmpty = userInput1==='' || userInput1===null || userInput1===''|| userInput1===null ;
+  let sum = Number(userInput1) + Number(userInput2);
+  if(checkEmpty || isNaN(sum)){
+    console.log('Error');
+  }else{
+    console.log(userInput1, '+' ,userInput2, '=' , sum);
+  }
+}
+
+getSumTwoNums();
