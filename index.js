fee25fa3ba5dbbaa5b3f8df9e37a2279f1ea49ac
Alena.Kygai <alena.kygai@gmail.com>
2021-10-07 17:27:11 +0300

    Example equality operators

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-// создать две переменные и выполнить с ними 5 арифметических операций с логированием
-let number_1 = 45;
-let number_2 = 12;
+/* 
+8 == 8 (равно)
+8 != 8 (не равно)
+8 === 8 (строгое равенство)
+8 !== 8 (строгое неравенство)
 
-console.log('result of + =', number_1+number_2);
-console.log('result of - =', number_1-number_2);
-console.log('result of * =', number_1*number_2);
-console.log('result of / =', number_1/number_2);
-console.log('result of % =', number_1 % number_2);
-
-// создать переменные со значениями
-let telNumber = 101;
-let boolValue = true;
-let nameCode = 'Java';
-let userName = 'Elon Musk';
-let emptyValue = null;
-
-let number = 23;
-number += 11; // number = number + 11
-number /= 2; // number = number / 2
-number--; // number = number - 1
-number *= 11; // number = number * 11
-number++; // number = number + 1
-number %= 5; // number = number % 5
+=== оператор строго равенства сравнивает еще и типы 
 
 
+в практике используем только строгое равенство/неравенство 
+
+*/
 
