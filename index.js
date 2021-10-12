e886016e7a66f30fdf830d55a22a0d69bf7182e2
Alena.Kygai <alena.kygai@gmail.com>
2021-10-12 16:22:42 +0300

    Add exampel hightOrderFunction and clear-function

--- index.js
+++ index.js
@@ -1,38 +1,59 @@
-/* 
-человек вводит номер месяца, а мы выводим время года
-1,2,12 -winter
-3,4,5 - spring
-6,7,8 - summer
-9,10,11 - autumn 
-*/
 
-const numberOfMounth = prompt('Enter number of mounth');
-
-switch (numberOfMounth) {
-  case '1':
-  case '2':
-  case '12':
-    console.log('Winter');
-    break;
-
-  case '3':
-  case '4':
-  case '5':
-    console.log('Spring');
-        break;
-  case '6':
-  case '7':
-  case '8':
-    console.log('Summer');
-         break;
-  case '9':
-  case '10':
-  case '11':
-     console.log('Autumn');
-      break;
-
-      default:
-        console.log('Invalid input');
-        break;
+const sum = function(a,b){
+  return a+b;
+}
+const sub = function(a,b){
+  return a-b;
+}
+const mult = function(a,b){
+  return a*b;
+}
+const div = function(a,b){
+  return a/b;
+}
+const rem = function(a,b){
+  return a%b;
 }
 
+// const calculate = function(num1=10,num2=2, operator='+'){
+//   if(isNaN(num1*num2)){
+//     return false;
+//   }
+//   let f;
+//   switch (operator) {
+//     case '+':
+//       f = sum; break;
+      
+//     case '-':
+//       f = sub; break; 
+
+//     case '*':
+//       f = mult; break;  
+
+//     case '/':
+//       f = div; break;     
+
+//     default:
+//       return null;
+//   }
+//   return f(num1,num2);
+// }
+
+// console.log(calculate());
+
+// const userNumber1 = prompt('Enter fisrt number');
+// const userNumber2 = prompt('Enter second number');
+// const userOperator = prompt('Enter operator');
+// console.log(calculate(userNumber1,userNumber2,userOperator));
+
+
+
+const hightOrderFunction = function(num1, num2, func){
+  return func(num1,num2);
+}
+
+const res = hightOrderFunction(2,6,mult);
+console.log(res);
+
+const res1 = hightOrderFunction(2,6,rem);
+console.log(res1);
