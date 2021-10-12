2293e44456eddc752630debe51ae968ee2c0d218
Alena.Kygai <alena.kygai@gmail.com>
2021-10-12 19:00:55 +0300

    Add operator while

--- index.js
+++ index.js
@@ -1,59 +1,31 @@
 
-const sum = function(a,b){
-  return a+b;
+let count = 0;
+while(count<5){
+    count++;
+    console.log(count);     // сначало прибавили. потом залогировали
 }
-const sub = function(a,b){
-  return a-b;
-}
-const mult = function(a,b){
-  return a*b;
-}
-const div = function(a,b){
-  return a/b;
-}
-const rem = function(a,b){
-  return a%b;
-}
+console.log('End loop');
 
-// const calculate = function(num1=10,num2=2, operator='+'){
-//   if(isNaN(num1*num2)){
-//     return false;
-//   }
-//   let f;
-//   switch (operator) {
-//     case '+':
-//       f = sum; break;
-      
-//     case '-':
-//       f = sub; break; 
-
-//     case '*':
-//       f = mult; break;  
-
-//     case '/':
-//       f = div; break;     
-
-//     default:
-//       return null;
-//   }
-//   return f(num1,num2);
+// let count = 0;
+// while(true){
+//     if(++count>5){
+//       break;
+//     }
+//     console.log(count);     
 // }
-
-// console.log(calculate());
-
-// const userNumber1 = prompt('Enter fisrt number');
-// const userNumber2 = prompt('Enter second number');
-// const userOperator = prompt('Enter operator');
-// console.log(calculate(userNumber1,userNumber2,userOperator));
+// console.log('End loop');
 
 
 
-const hightOrderFunction = function(num1, num2, func){
-  return func(num1,num2);
+let count = 0;
+while(true){
+    count++;
+    if(count%2===1){
+      continue;     // оператор перевода к началу новой итерраци
+    }
+    if(count>10){
+      break;
+    }
+    console.log(count);     
 }
-
-const res = hightOrderFunction(2,6,mult);
-console.log(res);
-
-const res1 = hightOrderFunction(2,6,rem);
-console.log(res1);
+console.log('End loop');
