6672876d96a09cbe71e8afa417d316c08026c20b
Alena.Kygai <alena.kygai@gmail.com>
2021-10-13 17:39:16 +0300

    Add example for

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+// объявление и инициализация счетчика итеррации
+// условие захода в каждый итеррацию
+for(let i=0; i<MAX_COUNT_TRY; i++){
+  console.log(i);
+  const password = prompt('Enter password');
+  if (password === RIGHT_PASSWORD){
+    alert('Thanks')
+    break;
+  }
+  if(i+1===MAX_COUNT_TRY){
+    alert('Try over');
+  }
+}
 
