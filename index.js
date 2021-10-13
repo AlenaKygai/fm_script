92a0b3416f42f15ed7188a5dc4b8d901ddcf113b
Alena.Kygai <alena.kygai@gmail.com>
2021-10-13 20:36:12 +0300

    Add tasks with objects

--- index.js
+++ index.js
@@ -36,3 +36,54 @@ const userVasia = new User('Vasia', 'Volskia', 35);
 const userMiha = new User('Miha', 'Kalugin', 27);
 
 console.log(userMiha.fullName());
+
+//создать функцию конструктор Country
+//  name,population,area 
+//создать Country
+//создать метод который возвращает плотность населения 
+
+function Country(name,population,area){
+  this.name = name;
+  this.population = population;
+  this.area = area;
+  this.getDensity = function(){
+    return this.population/this.area;
+  }
+}
+
+const countyUkraine = new Country('Ukraine',43733762,579320);
+console.log(countyUkraine.getDensity());
+const countryItaly = new Country('Italy', 60461826, 294140);
+console.log(countryItaly.getDensity());
+
+// Car
+
+function Auto(name,maxSpeed){
+  this.name = name;
+  this.maxSpeed = maxSpeed;
+  this.speed = 0;
+
+  this.accelerate = function(value){
+    this.speed +=value; 
+    if(this.speed>maxSpeed){
+      this.speed = maxSpeed;
+    }
+    return this.speed;
+  }
+  this.deaccelerate = function(value){
+    this.speed -= value; 
+    if(this.speed<0){
+      this.speed = 0;
+    }
+    return this.speed;
+  }
+  this.stop = function(){
+    return (this.speed=0);
+  }
+}
+
+const car = new Auto('bmw', 100);
+
+console.log(car.deaccelerate(50));
+console.log(car.accelerate(150));
+
