aeb44867d415b61a1c5c9c5ce18a70ab2fcb4feb
Alena.Kygai <alena.kygai@gmail.com>
2021-10-13 19:18:12 +0300

    Exampel creat object

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
-// объявление и инициализация счетчика итеррации
-// условие захода в каждый итеррацию
-// for(let i=0; i<MAX_COUNT_TRY; i++){
-//   console.log(i);
-//   const password = prompt('Enter password');
-//   if (password === RIGHT_PASSWORD){
-//     alert('Thanks')
-//     break;
-//   }
-//   if(i+1===MAX_COUNT_TRY){
-//     alert('Try over');
-//   }
-// }
+const object = {
+  display: 'flex',
+  margin:'15px',
+  justifyContent:'center',
+  visibility: true,
+  resize:function(){
+    return 'resize';
+  },
+};
 
-// найти факториал числа 
+const ob1 = {};
 
-
-const calcFactorial = function(number=2){
-  if(number<0){
-    return null;
-  }
-  if(number===0 || number===1){
-    return 1;
-  }
-  if(number>MAX_FACTORIAL){
-    return false;
-  }
-  let result = 1n;
-  for(let i=1; i<=number; i++){
-    result *= BigInt(i);
-  }
-  return result;
-}
-console.log(calcFactorial(3));
-console.log(calcFactorial(4));
+const ob1 = Object();
+const ob1 = new Object();
