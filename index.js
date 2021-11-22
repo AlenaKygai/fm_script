cb44e2b5ea04f7af65978f4e88ec3ad240ed6233
Alena.Kygai <alena.kygai@gmail.com>
2021-11-22 21:18:13 +0300

    Practic access to a computable property

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-const obj = {
-  name:'pen',
-  'is writen': true, // строка может быть именем свойства 
-  11:78,
+//таблица умнажения, где пример это свойста , 
+//  значение- это решение этого примера
+
+// const table = {
+//   '2 * 2 =' : 4,
+//   '2 * 3 =' : 6,
+// }
+
+const obj = {};
+obj.prop1 = 12;
+obj['prop2'] = 22;
+
+const craeteMultiplicationTable = function(min,max){
+  const table = {};
+  for(let i=min; i<=max; i++){
+    for(let j=1; j<=max; j++){
+      //доступ к свойству с возможностью вычисления
+      table[`$(i) * $(j) = `] = i*j;
+    }
+  }
+  return table;
+}
+console.log(craeteMultiplicationTable());
+
+
+const getNumber = function(str,obj){
+  return obj[str];
 }
 
-//доступ к свойству с возможностью вычисления , в квадратных скобках происходит
-// вычисления переданного значения
-const nameProperty = 'is writen';
-console.log(obj[11]);
-console.log(obj['11']);
-console.log(obj['is writen']); 
-console.log(obj['name']);
-console.log(obj['nameProperty']);
-
+const str = '2 * 3 =';
+const table = craeteMultiplicationTable();
+console.log(getNumber(str,table));
