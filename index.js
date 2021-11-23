b4f3cfe1291fbc39bf0e859d3ffbda750547e574
Alena.Kygai <alena.kygai@gmail.com>
2021-11-23 21:39:56 +0300

    Add MyArray and .push

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-// const obj1 = {
-//   name: 'obj1',
-// };
-// const obj2 = new Object({name: 'obj2'});
+function MyArray() {
+  this.length = 0;
 
-const phArray = new Array('271-45-45', '271-55-45', '271-45-77');
-const phArray2 = ['271-45-45', '271-55-45', '271-45-77'];
+  this.push = function(item) {
+    this[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+}
+
+const myArray = new MyArray();
+const myArray2 = new MyArray();
+console.log(myArray.push===myArray2.push); // false
 
 
+const arr = [];
+arr.push(1);
+
+const arr2 = [];
+console.log(arr.push===arr2.push);  // true
