b63b1e463ebca40f253918c2f2d379f410498554
Alena.Kygai <alena.kygai@gmail.com>
2021-11-23 18:56:20 +0300

    Add call constructor without NEW

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const t1 = 1;
-const t2 = 1;
-
 const obj1 = {};
-const obj2 = {};
+const obj2 = Object();
+const obj3 = new Object();
 
-const link = obj1; //копирование по ссылке
+console.log(obj1);
+console.log(obj2);
+console.log(obj3);
 
-console.log(obj1==link); // true
-console.log(obj2==link); // false
 
-link.prop2 = 44; 
+//способ создавать объекты, без директивы NEW
+//the same like this: const obj2 = Object();  WITHOUT OPERATOR NEW
+function MyObj() {
+  //new - создает пустой объект для функции конструктора
+  //новый объект - this
+  if(!new.target){
+    return new MyObj();
+  }
+}
+
+//WITHOUT OPERATOR(директива) NEW
+const myObj = MyObj();
+console.log(MyObj);
