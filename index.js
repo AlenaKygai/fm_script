98c7fe4739e0a681f86ad6ddaf01c7c4b3a839e2
Alena.Kygai <alena.kygai@gmail.com>
2021-11-23 22:25:56 +0300

    Add prototype

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-function MyArray() {
-  this.length = 0;
-
+/* logic  */
+function MyArrayProto(){
   this.push = function(item) {
     this[this.length] = item;
-    this.length++;
-    return this.length;
+    // this.length++;
+    return ++this.length;
   }
 }
+/* data */
+function MyArray() {
+  this.length = 0;
+}
+/* Protopype */
+MyArray.prototype = new MyArrayProto();
+
 
 const myArray = new MyArray();
 const myArray2 = new MyArray();
-console.log(myArray.push===myArray2.push); // false
+// console.log(myArray);
+// console.log(myArray.push===myArray2.push); // true
 
 
 const arr = [];
 arr.push(1);
-
+// console.log(arr);
 const arr2 = [];
-console.log(arr.push===arr2.push);  // true
+// console.log(arr.push===arr2.push);  // true
