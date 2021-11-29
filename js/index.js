0d570e6ef3b8d30a7d91bd0f37f6cbc10157c7b1
Alena.Kygai <alena.kygai@gmail.com>
2021-11-30 00:16:58 +0300

    Add prototype for constructor

--- js/index.js
+++ js/index.js
@@ -1,50 +1,46 @@
 
-function toString() {
-  return `${this.firstName} ${this.lastName} `;
-}
-
-const animal = {
-  go: function () {
+function StudentPrototype (){
+  this.toString = function() {
+    return `${this.firstName} ${this.lastName} `;
+  }
+  this.go = function () {
     return `${this.firstName} go`;
   }
-}
-
-const man = {
-  eat: function () {
+  this.eat = function () {
     return `${this.firstName} eating`;
   }
 }
+/**
+ * 
+ * @param {string} firstName 
+ * @param {string} lastName 
+ * @param {number} age 
+ * @param {boolean} isMale 
+ * @returns instance Student
+ */
+function Student (firstName, lastName, age, isMale){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.isMale = isMale;
+  if(!new.target){
+    return new Student(firstName, lastName, age, isMale);
+  }
+}
 
-man.__proto__ = animal;
-// литеральный способ
-// {} - это синтаксический сахар в данном контексте, все равно что обращаемся к встроенному конструктору и создаем объект
-// const student1 = new Object({})
-const student1 = {
-  id:1,
-  firstName: 'Elon',
-  lastName: 'Musk',
-  age:50,
-  isMale: true,
-// если свойство является методом, то можно просто назначить этот метод
-// toString: toString,
-  toString,
-};
+Student.prototype = new StudentPrototype();
 
+const student1 = Student('Elon', 'Musk', 40, true);
 console.log(student1.toString());
-student1.__proto__ = man; // для наследования от литирального объекта к литеральному объекту другому 
-console.log(student1.eat());
 console.log(student1.go());
+console.log(student1.eat());
 
-const student2 = {
-  id:2,
-  firstName: 'Tim',
-  lastName: 'Li',
-  age:65,
-  isMale: true,
 
-  toString: toString,
-};
-console.log(student2.toString());
+
+
+
+
+
 
 
 
