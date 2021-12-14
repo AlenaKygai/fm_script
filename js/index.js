4b04c1662c70b3ea1e1578a6d308aaf0f430b7ca
Alena.Kygai <alena.kygai@gmail.com>
2021-12-14 19:45:13 +0300

    Add bind

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
 'use strict';
+function logItem(header,index){
+  console.log(this.title);
+  console.log(`${index+1}: ${header}`);
+};
 
 const site = {
   title:'Green site',
   headers:['Header1','Header2','Header3','Header4'],
   showHeaders(){
-    this.headers.forEach(function (header,index){
-      console.log(this.title);
-      console.log(`${index+1}: ${header}`);
-    },this);
+    this.headers.forEach(logItem.bind(this));
   }
 };
 
