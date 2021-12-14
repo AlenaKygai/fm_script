77acdbe01806490b15eb3b303a4f5292699fa64f
Alena.Kygai <alena.kygai@gmail.com>
2021-12-14 19:31:52 +0300

    Add thisArg for forEach

--- js/index.js
+++ js/index.js
@@ -4,17 +4,10 @@ const site = {
   title:'Green site',
   headers:['Header1','Header2','Header3','Header4'],
   showHeaders(){
-    const that = this;
-    this.headers.forEach((header,index) => {
-      console.log(that.title);
+    this.headers.forEach(function (header,index){
+      console.log(this.title);
       console.log(`${index+1}: ${header}`);
-
-      // const test = ()=>{
-      //   console.log(this);
-      // }
-      // test();
-
-    });
+    },this);
   }
 };
 
