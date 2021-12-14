84592b9d4527c466f828f967e0b45d2242bb36a7
Alena.Kygai <alena.kygai@gmail.com>
2021-12-14 22:55:54 +0300

    Add spread example

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 // sum, которая принимает неограничинное  кол-во параметров
 
-const getSum = (...args) =>{
+const getSum = (...args) =>{ //rest 
   let result = null;
   args.forEach((n) => {
      result += n ;
@@ -15,5 +15,9 @@ console.log('result: ' + getSum(1,2,3,4,5));
 const sum2 = (...args) => args.reduce( (result,n) => result+n);
 
 const arrNums1 = [4,1,3,6];
-console.log(getSum(...arrNums1));
+const arrNums2 = [4,9,3,6];
+console.log(getSum(...arrNums1)); //spread
 console.log(sum2(...arrNums1));
+
+const arr3 = [...arrNums1, ...arrNums2];
+console.log(arr3);
