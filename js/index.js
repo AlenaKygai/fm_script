df1ed3961e816fc3fe33df66fd2efa1a044acb7a
Alena.Kygai <alena.kygai@gmail.com>
2021-12-14 22:04:59 +0300

    Add example function with rest parametrs

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 'use strict';
-const t1 = function(){
+const t1 = function(...rest){
   console.log(arguments)
+  console.log(rest)
 }
 
-const t2 = () => {
-  console.log(arguments)
+const t2 = (num, ...rest) => {
+  // console.log(arguments)
+  console.log('num = ', num)
+  console.log('rest = ', rest)
 }
 
 console.dir(t1);
@@ -12,3 +15,14 @@ console.dir(t2);
 
 t1(1,2,3,4);
 t2(1,2,3,4);
+
+// sum, которая принимает неограничинное  кол-во параметров
+
+const getSum = (...args) =>{
+  let result = null;
+  args.forEach(element => {
+     result += element ;
+  });
+  return result;
+}
+console.log('result: ' + getSum(1,2,3,4,5));
