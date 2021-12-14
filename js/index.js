e94945c8879b1af6e6d2b2b15a5a9d0b6e155143
Alena.Kygai <alena.kygai@gmail.com>
2021-12-14 18:57:13 +0300

    Add arrow function

--- js/index.js
+++ js/index.js
@@ -1,62 +1,23 @@
+'use strict';
 
-function User(firstName, lastName, age, isMale, email, isSubscribe = false){
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.isMale = isMale;
-  this.email = email;
-  this.isSubscribe = isSubscribe;
+
+// декларативное объявление функции 
+function test1(){
+  console.log(this);
 }
 
-function UserPrototype(){
-  this.fullName = function(){
-    return `${this.firstName} ${this.lastName}`;
-  }
+//выражение  
+// нельзя вызвать перед объявлением функции 
+const test2 = function(){
+  console.log(this);
 }
 
-User.prototype = new UserPrototype();
 
-function createRandomUsers(amount = 1){
-  const db = [];
-  for(let i=0; i < amount; i++){
-    const user = new User(
-      `Name${i}`,
-      `LName${i}`,
-      Math.ceil(Math.random()*60+20), // 20-80 years
-      Math.random()>0.5 ,  // true || false
-      `email${i}@gmail.com`
-    );
-    db.push(user);
-  }
-  return db;
+//стрелка, берет this ищет из окружения, у нее нет своего контекста
+const test3 = () => {
+  console.log(this);
 }
 
-const users = createRandomUsers(100);
-users.forEach(function(user){
-  user.isSubscribe = (Math.random()> 0.5);
-});
-console.table(users);
-
-/*получить массив полных имен полльхзователей, используя метод map */
-const fullNameUsers = users.map(function(user){
-  return user.fullName();
-});
-console.table(fullNameUsers);
-
-
-/*получить массив пользователей, которые старше 65 */
-function olderPerson(user){
-  return user.age >= OLD_AGE;
-}
-
-const oldUsers = users.filter(olderPerson);
-console.table(oldUsers);
-
-/*получить массив пользователей, до 40 лет, женского пола у которого есть подписка */
-
-function getSpecialFilter(user){
- return  user.age < 40 && !user.isMale && user.isSubscribe;
-}
-
-const newWomen = users.filter(getSpecialFilter);
-console.table(newWomen);
+test1();
+test2();
+test3();
