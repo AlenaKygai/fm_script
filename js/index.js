50f73d8de1be32dd31fc32131a5d77e6cf75bf29
Alena.Kygai <alena.kygai@gmail.com>
2021-12-14 23:05:20 +0300

    Add recursion

--- js/index.js
+++ js/index.js
@@ -1,23 +1,12 @@
 'use strict';
 
-// sum, которая принимает неограничинное  кол-во параметров
+function recursion(num){
 
-const getSum = (...args) =>{ //rest 
-  let result = null;
-  args.forEach((n) => {
-     result += n ;
-  });
-  return result;
+if(num === 0){
+  return;
 }
-console.log('result: ' + getSum(1,2,3,4,5));
+console.log(num);
 
-
-const sum2 = (...args) => args.reduce( (result,n) => result+n);
-
-const arrNums1 = [4,1,3,6];
-const arrNums2 = [4,9,3,6];
-console.log(getSum(...arrNums1)); //spread
-console.log(sum2(...arrNums1));
-
-const arr3 = [...arrNums1, ...arrNums2];
-console.log(arr3);
+recursion(num-1);
+}
+recursion(8);
