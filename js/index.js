2fb973a4286448addb4345456c39ce7bc4b4de50
Alena.Kygai <alena.kygai@gmail.com>
2021-12-14 20:49:09 +0300

    Add arrow this

--- js/index.js
+++ js/index.js
@@ -8,7 +8,10 @@ const site = {
   title:'Green site',
   headers:['Header1','Header2','Header3','Header4'],
   showHeaders(){
-    this.headers.forEach(logItem.bind(this));
+    this.headers.forEach((header,index) => {
+      console.log(this.title);
+      console.log(`${index+1}: ${header}`);
+    });
   }
 };
 
