76931208e67039e2fc54d6b146d14480aeb73090
Alena.Kygai <alena.kygai@gmail.com>
2021-12-15 23:24:48 +0300

    Add get and set

--- js/newSyntax.js
+++ js/newSyntax.js
@@ -8,46 +8,55 @@ class UserClass{
    * @param {number} age  0 - 150 
    */
   constructor(name,sname,age){
+    this.name = name;
+    this.sname = sname;
+    this.age = age; // setter
+  }
+  get age(){
+    return this._age;
+  }
+  set age(age){
     if(typeof age!=='number'){
       throw new TypeError('Age must be number')
     }
     if(age < 0 || age> MAX_AGE){
-      throw new RangeError(`Age must be > 0 and < ${MAX_AGE}`)
+      throw new RangeError(`Age must be > 0 and < ${MAX_AGE} `)
     }
-    this.name = name;
-    this.sname = sname;
-    this.age = age;
+    this._age = age;
   }
   getFullName(){
     return `${this.name} ${this.sname}`;
   }
 
   checkAdult(){
-    return this.age >= ADULT_AGE;
+    return this._age >= ADULT_AGE;
   }
 }
 
 const u2 = new UserClass('Tim', 'Li', 30);
 
-console.log(u2.checkAdult());
+// console.log(u2.checkAdult());
+u2.age = 48; // setter
+console.log(u2.age); // getter
 
-class Worker{
-  /**
-   * 
-   * @param {string} name 
-   * @param {string} sname 
-   * @param {number} rate 
-   * @param {number} wdays 
-   */
-  constructor(name,sname,rate,wdays){
-    this.name = name;
-    this.sname = sname;
-    this.rate = rate;
-    this.wdays = wdays;
-  }
-  getSalary(){
-    return this.rate*this.wdays;
-  }
-}
-const w1 = new Worker('Alena', 'Kygai','10','10');
-console.log(w1.getSalary());
+
+// class Worker{
+//   /**
+//    * 
+//    * @param {string} name 
+//    * @param {string} sname 
+//    * @param {number} rate 
+//    * @param {number} wdays 
+//    */
+//   constructor(name,sname,rate,wdays){
+//     this.name = name;
+//     this.sname = sname;
+//     this.rate = rate;
+//     this.wdays = wdays;
+//   }
+//   getSalary(){
+//     return this.rate*this.wdays;
+//   }
+// }
+// const w1 = new Worker('Alena', 'Kygai','10','10');
+// console.log(w1.getSalary());
