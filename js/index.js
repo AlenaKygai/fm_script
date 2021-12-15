efda8e3fe18e2e67a4dd421df95da74327060323
Alena.Kygai <alena.kygai@gmail.com>
2021-12-15 19:19:26 +0300

    Add new Number

--- js/index.js
+++ js/index.js
@@ -1,18 +1,12 @@
 'use strict';
 
-const boolean = new Boolean (false); // одна из возможностей - явное прeведение типа
-console.log(boolean); // object
+const number = new Number (10); // создание объекта Number
+console.dir(number);
+console.dir(123123.456789.toFixed(2));
 
-if(boolean){
-  console.log('true')
-}else{
-  console.log('false')
-}
+const num = Number(123123.456789.toFixed(2));
+console.log(num);
 
-
-const str = 'Elon';
-if(Boolean(str)){
-  console.log(Boolean(str))
-}else{
-  console.log('false')
-}
+let num2 = 12.123456; // объектом-оберткой через конструктор Number()
+//num2 = num2.toFixed(2);
+console.dir(num2);
