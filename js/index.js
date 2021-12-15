6c132c197442f837803a6706bf8774ffebe9c81b
Alena.Kygai <alena.kygai@gmail.com>
2021-12-15 21:20:05 +0300

    Add new Error

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 'use strict';
 
-// //SyntaxError
-// if 5>2 
-//   console.log(); 
-
-// //ReferenceError
-// user;
-// let user;
+const pow = (num,exp) =>{
+  if(typeof num !== 'number' || typeof exp !=='number'){
+    return new TypeError('Value must be number');
+  }
+  if(exp<0){
+    return new RangeError('Invalid value exp. Exp must > 0 ');
+  }
+  if(exp === 0){
+    return 1;
+  }
+  return num*pow(num,exp-1);
+}
+console.log(pow(2,-2)); // result NaN
