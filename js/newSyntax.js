4c21a91162c92826b402aaf545aad7b3f68ccb0e
Alena.Kygai <alena.kygai@gmail.com>
2021-12-15 23:00:45 +0300

    Add validate data

--- js/newSyntax.js
+++ js/newSyntax.js
@@ -1,7 +1,19 @@
 'use strict';
 
 class UserClass{
+  /**
+   * 
+   * @param {string} name 
+   * @param {string} sname 
+   * @param {number} age  0 - 150 
+   */
   constructor(name,sname,age){
+    if(typeof age!=='number'){
+      throw new TypeError('Age must be number')
+    }
+    if(age < 0 || age> MAX_AGE){
+      throw new RangeError(`Age must be > 0 and < ${MAX_AGE}`)
+    }
     this.name = name;
     this.sname = sname;
     this.age = age;
@@ -18,3 +30,24 @@ class UserClass{
 const u2 = new UserClass('Tim', 'Li', 30);
 
 console.log(u2.checkAdult());
+
+class Worker{
+  /**
+   * 
+   * @param {string} name 
+   * @param {string} sname 
+   * @param {number} rate 
+   * @param {number} wdays 
+   */
+  constructor(name,sname,rate,wdays){
+    this.name = name;
+    this.sname = sname;
+    this.rate = rate;
+    this.wdays = wdays;
+  }
+  getSalary(){
+    return this.rate*this.wdays;
+  }
+}
+const w1 = new Worker('Alena', 'Kygai','10','10');
+console.log(w1.getSalary());
