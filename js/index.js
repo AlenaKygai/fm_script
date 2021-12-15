f37814c73409d4400a86794bbc85268779f53d61
Alena.Kygai <alena.kygai@gmail.com>
2021-12-15 20:21:51 +0300

    Add Type Error

--- js/index.js
+++ js/index.js
@@ -1,35 +1,9 @@
 'use strict';
 
-/*
-функция которая приводит строку к такому кейсу 'To be or Not to be'
-*/
+// //SyntaxError
+// if 5>2 
+//   console.log(); 
 
-const str = 'to BE or NoT TO Be';
-  
-function getCapitalizeStr(str){
-  const arrWords = str.split(' ');
-  const arrWordsUp = arrWords.map((word)=>{
-    if(word){
-      return word[0].toUpperCase() + word.substring(1).toLowerCase();
-    }
-    return;
-  });
-  return arrWordsUp.join(' '); 
-}
-
-console.log(getCapitalizeStr(str));
-
-
-/*
-function getCapitalizeStr(str){
-  return str
-  .split(' ')
-    .map((word)=>{
-      if(word){
-        return word[0].toUpperCase() + word.substring(1).toLowerCase();
-      }
-  })
-  return arrWordsUp.join(' '); 
-}
-console.log(getCapitalizeStr(str));
-*/
+// //ReferenceError
+// user;
+// let user;
