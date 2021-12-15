0b2a0a948bd10eccd6c1e93947b951560cff6e40
Alena.Kygai <alena.kygai@gmail.com>
2021-12-15 19:39:25 +0300

    Add example metodth with string

--- js/index.js
+++ js/index.js
@@ -1,12 +1,28 @@
 'use strict';
 
-const number = new Number (10); // создание объекта Number
-console.dir(number);
-console.dir(123123.456789.toFixed(2));
+const userName = 'Elon';
 
-const num = Number(123123.456789.toFixed(2));
-console.log(num);
+console.log(userName.toLocaleUpperCase());
 
-let num2 = 12.123456; // объектом-оберткой через конструктор Number()
-//num2 = num2.toFixed(2);
-console.dir(num2);
+console.log(userName[2]);
+
+console.log(userName.charAt(3));
+
+console.log(userName.charCodeAt(2));
+
+
+
+const str = 'Сегодня хороший день у Илона Маска';
+console.log(str.includes('я'));
+console.log(str.includes('день'));
+console.log(str.indexOf('день'));
+
+
+const str1 = '      qwewq     ';
+console.log(str1.trim());
+
+const str2 = 'To be or not to be';
+console.log(str2.split());
+console.log(str2.split(''));
+console.log(str2.split(' '));
+console.log(str2.split('or'));
