c3f8c8ee765ad4bc85828605897974ab38f140a3
Alena.Kygai <alena.kygai@gmail.com>
2021-12-17 20:15:58 +0300

    Add flat

--- js/MyArrayClass.js
+++ js/MyArrayClass.js
@@ -53,6 +53,43 @@ every(func){
     }
     return result;
   }
+  concat(myArrayInstance){
+    const newMyArray = new MyArray();
+    for (let i = 0; i < this.length; i++) {
+      newMyArray.push(this[i]);
+    }
+    for (let i = 0; i < myArrayInstance.length; i++) {
+      newMyArray.push(myArrayInstance[i]);
+    }
+    return newMyArray;
+  }
+//   flat(depth=1){
+//     let result = new MyArray();
+//     for (let i = 0; i < this.length; i++) {
+//       const isArray = MyArray.isArray(this[i]);
+//       if(isArray && depth){
+// /*flat */
+//         const subResult = this[i].flat(depth-1);
+//         result = result.concat(subResult);
+//       }else if(this[i]!==undefined){
+//         result.push(this[i]);
+//       }
+//     }
+//     return result;
+//   }
+flat(depth=1){
+  let result = new MyArray();
+  this.forEach((item) =>{
+    if(MyArray.isArray(item) && depth){
+        result = result.concat(item.flat(depth-1));
+      }else if(item!==undefined){
+        result.push(item);
+      }
+  });
+  return result;
+}
+
+
   static isArray(obj){
   return obj instanceof MyArray;
   }
