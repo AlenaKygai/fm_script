85b17f26d01e3ab8a6867b71364d1b1a8095a783
Alena.Kygai <alena.kygai@gmail.com>
2021-12-17 23:02:15 +0300

    Add for of

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
 'use strict';
 
-const MyArray1 = new MyArray(1,undefined,1,new MyArray(2,2),1,1);
-const MyArray2 = MyArray1.flat(1);
-console.log(MyArray2);
+// const MyArray1 = new MyArray(1,undefined,1,new MyArray(2,2),1,1);
+// const MyArray2 = MyArray1.flat(1);
+// console.log(MyArray2);
+
+const mySumbol1 = Symbol('Property');
+
+const object = {
+  login:'qwerty',
+  mySumbol1: 'asda',
+  [mySumbol1]: 123,
+}
+
+const arr = [1,2,3,4,5];
+for (const iterator of arr) {
+  console.log(iterator);
+}
