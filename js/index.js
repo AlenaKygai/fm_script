8380357d6959ccc7bd5f3ffb02fa5ee6bdb9ce00
Alena.Kygai <alena.kygai@gmail.com>
2021-12-19 00:51:07 +0300

    Add BIG O

--- js/index.js
+++ js/index.js
@@ -4,19 +4,54 @@
 // const MyArray2 = MyArray1.flat(1);
 // console.log(MyArray2);
 
-const mySumbol1 = Symbol('Property');
+const arr = [1,5,3,5,89,8,4,56,8,9,2,1,5];
+// константная сложность O(1)
+console.log(arr[4]);
 
-const object = {
-  login:'qwerty',
-  mySumbol1: 'asda',
-  [mySumbol1]: 123,
+//линейная сложность  
+const linerSerach = (arr,key) =>{
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] === key){
+      return i;
+    }
+  }
+  return -1;
 }
+console.log(linerSerach(arr,5)); // result 1
+console.log(linerSerach(arr,55)); // result -1
+console.log(linerSerach(arr,89)); // result 4
 
-const arr = [1,2,3,4,5];
-for (const iterator of arr) {
-  console.log(iterator);
+//квадратичная сложность O(n^2)
+const createTableMult = (limit) =>{
+  const table = [];
+    for (let i = 1; i <= limit; i++) {
+      for (let j = 1; j <= limit; j++) {
+        table.push(`${i}*${j} = ${i*j}`);
+      }
+    }
+  return table;
 }
+console.log(createTableMult(5));
 
-// console.log(...arr);
-const myArray2 = new MyArray(10,20,30);
-console.log(...myArray2);
+
+//логарифмическая сложность O(logn)
+const sortedArr = [1,3,5,8,12,15,19,40];
+
+const binarySearch = (arr, key) =>{
+  let start = 0;
+  let end = arr.length-1;
+  let middle;
+  while(start <= end ){
+    middle = Math.round((start + end)/2);
+    if(arr[middle] === key){
+      return middle;
+    }
+    if(arr[middle] < key){
+      start = middle + 1;
+    }else{
+      end = middle - 1;
+    }
+  }
+  return -1;
+}
+console.log(binarySearch(sortedArr,15));
